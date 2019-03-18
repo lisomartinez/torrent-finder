9f9166a42187c54b83afe369eee67816b4811b8b
Lisandro Martinez <lisandromartinez@gmail.com>
2019-03-18 13:41:09 -0300

    Initial Commit

---
+++ src/main/java/com/martinez/lisandro/torrentfinder/TorrentFinderApplication.java
@@ -0,0 +1,13 @@
+package com.martinez.lisandro.torrentfinder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TorrentFinderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TorrentFinderApplication.class, args);
+	}
+
+}
